8b27467755b073dc5d93f8adcf04b3c8f75e2c9b
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-22 15:48:32 +0530

    Added file

---
+++ User_Registration/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace User_Registration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name :");
+            string name = Console.ReadLine();
+
+            User user = new User();
+            user.FirstName(name);
+        }
+    }
+}
---
+++ User_Registration/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    public class User
+    {
+        public void FirstName(string name)
+        {
+            Regex firstName = new Regex(@"^([A-z]{1}[a-z]{2,})$");
+            bool matches = firstName.IsMatch(name);
+
+            if(matches == true)
+            {
+                Console.WriteLine($"{name} is Valid");
+            }
+            else
+            {
+                Console.WriteLine($"{name} is InValid");
+            }
+        }
+    }
+}
